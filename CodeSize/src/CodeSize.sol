8a0c021dfab91fd9eff20ce1dff20adf03220bee
oderahub <okere_chidera@yahoo.com>
2025-08-27 02:37:01 +0100

    chore: fix

--- CodeSize/src/CodeSize.sol
+++ CodeSize/src/CodeSize.sol
@@ -5,4 +5,44 @@ contract CodeSize {
     /**
      * The challenge is to create a contract whose runtime code (bytecode) size is greater than 1kb but less than 4kb
      */
+
+    enum Status {
+        ACTIVE,
+        TERMINATED,
+        LEAVE
+    }
+
+    struct Mentor {
+        string name;
+        uint256 joinedTime; 
+        Status status;
+        bool hasAccess;
+    }
+
+    error NOT_OWNER();  
+
+    address[] public mentorsAddress;  
+
+    mapping(address => Mentor) public mappedMentors;
+
+    function createMentor(string memory _name) external { 
+        mappedMentors[msg.sender] = Mentor({
+            name: _name,  
+            joinedTime: block.timestamp,  
+            status: Status.ACTIVE,  
+            hasAccess: true
+        });
+
+        mentorsAddress.push(msg.sender);  // Added semicolon
+    }
+
+    function updateMentor(address _mentorAddress, Status _status) external {  // Fixed parameter syntax
+        if (msg.sender != _mentorAddress) {  // Fixed comparison operator
+            revert NOT_OWNER();
+        }
+
+        mappedMentors[_mentorAddress].status = _status;
+    }
+
+  
 }
